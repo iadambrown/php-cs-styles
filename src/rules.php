2c2527ea345abc0333ef0c8f1b8493d839f4e7ea
Adam Brown <adam.brown@factorybucket.com>
2020-05-05 13:02:19 -0400

    binary_operator_spaces

--- src/rules.php
+++ src/rules.php
@@ -7,4 +7,5 @@ return [
     'array_syntax' => ['syntax' => 'short'],
     'final_class' => false,
     'new_with_braces' => true,
+    'binary_operator_spaces' => ['align_double_arrow' => 'true', 'align_equals' => 'true'],
 ];
